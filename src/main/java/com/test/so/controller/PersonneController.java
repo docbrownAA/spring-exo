7140771ac963388bd97bf06b6af2b2e0c4d24aa2
DUVINAGE <gduvinage@gmail.com>
2017-04-19 17:26:55 +0200

    attempt to refacto

--- src/main/java/com/test/so/controller/PersonneController.java
+++ src/main/java/com/test/so/controller/PersonneController.java
@@ -24,28 +24,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/personnes")
 public class PersonneController {
-    
+
     @Autowired
     PersonneService personneService;
-    
+
     @Autowired
     PersonneDetailService personneDetailService;
-    
+
     @RequestMapping(method = RequestMethod.GET, value = "/personne", params = {"nom", "prenom"})
     public Personne readPersonne(@RequestParam("nom") String nom, @RequestParam("prenom") String prenom) {
         return personneService.findByNomAndPrenom(nom, prenom);
     }
-    
+
     @RequestMapping(method = RequestMethod.GET)
     public Collection<Personne> readPersonnes() {
         return personneService.findAll();
     }
-    
+
     @RequestMapping(method = RequestMethod.GET, value = "/{id_personne}")
     public Personne readOnePersonne(@PathVariable("id_personne") int idPersonne) {
         return personneService.findById(idPersonne);
     }
-    
+
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
             produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity createPersonne(@RequestBody Personne personne) {
@@ -59,37 +59,35 @@ public class PersonneController {
         }
         return new ResponseEntity(personneService.update(persNew), HttpStatus.OK);
     }
-    
+
     @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
             produces = {MediaType.APPLICATION_JSON_VALUE}, value = "/{id_personne}")
     public ResponseEntity updatePersonne(@PathVariable("id_personne") Integer id_personne, @RequestBody Personne personne) {
         Personne currPersonne = personneService.findById(id_personne);
-        currPersonne.setNom(personne.getNom());
-        currPersonne.setPrenom(personne.getPrenom());
-        currPersonne.setDate_naissance(personne.getDate_naissance());
-        PersonneDetail personneDetaiCurr = personneDetailService.findByPersonne(personne);
-        PersonneDetail personneDetail = personne.getPersonneDetail();
-        if (personneDetaiCurr == null) {
-            if (personneDetail != null) {
-                personneDetail.setPersonne(currPersonne);
-                PersonneDetail pdNew = personneDetailService.create(personneDetail);
+        currPersonne = managePersonne(currPersonne, personne);
+
+        PersonneDetail persDetFromDB = personneDetailService.findByPersonne(personne);
+        PersonneDetail persDetFromCli = personne.getPersonneDetail();
+        if (persDetFromDB == null) {
+            if (persDetFromCli != null) {
+                persDetFromCli.setPersonne(currPersonne);
+                PersonneDetail pdNew = personneDetailService.create(persDetFromCli);
                 currPersonne.setPersonneDetail(pdNew);
             }
         } else {
-            personneDetaiCurr.setNumSecu(personneDetail.getNumSecu());
-            personneDetaiCurr.setSexe(personneDetail.getSexe());
-            PersonneDetail pdUp = personneDetailService.save(personneDetaiCurr);
+            persDetFromDB.setNumSecu(persDetFromCli.getNumSecu());
+            persDetFromDB.setSexe(persDetFromCli.getSexe());
+            PersonneDetail pdUp = personneDetailService.save(persDetFromDB);
             currPersonne.setPersonneDetail(pdUp);
         }
-        //currPersonne.setPersonneDetail(personneDetail);
         return new ResponseEntity(personneService.update(currPersonne), HttpStatus.OK);
     }
-    
+
     @RequestMapping(method = RequestMethod.DELETE, value = "/personne/{id_personne}")
     public ResponseEntity delete(@PathVariable("id_personne") int idPersonne) {
         Personne personne = personneService.findById(idPersonne);
         if (personne == null) {
-            System.out.println("Impossible de tourver la personne:" + idPersonne);
+            System.out.println("Impossible de trouver la personne:" + idPersonne);
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
         PersonneDetail pdToDelete = personne.getPersonneDetail();
@@ -100,5 +98,14 @@ public class PersonneController {
         personneService.delete(personne);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
-    
+
+    private Personne managePersonne(Personne fromDataBase, Personne fromClient) {
+            fromDataBase.setNom(fromClient.getNom());
+            fromDataBase.setPrenom(fromClient.getPrenom());
+            fromDataBase.setDate_naissance(fromClient.getDate_naissance());
+
+        return fromDataBase;
+
+    }
+
 }
